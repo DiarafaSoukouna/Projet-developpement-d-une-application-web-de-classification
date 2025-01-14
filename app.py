80f7000dac83498dffbd9ce654d0aae23c5bc4b7
Diarafa Soukouna <diarafasouk@gmail.com>
2025-01-14 20:15:28 +0000

    Nouvel ajout

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
+file = 'index.html'
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return 'Hello World!'
+  return render_template('./index.html')
 
-if __name__ == '__main__':
-  app.run()
+if __name__ == '__main__': 
+  app.run(debug=True)
