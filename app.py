2c9efbded7c10ca3cc4541cf1206e8d71f69fcc3
Mohamed <mohamedmoustaphatr@gmail.com>
2025-01-20 16:04:47 +0000

    Upload file

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, jsonify, redirect, url_for, flash
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import pymysql
+ALLOWED_EXTENSIONS = {'docs', 'txt', 'doc'}
 app = Flask(__name__)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # Répertoire contenant app.py
+app.config['UPLOAD_FOLDER'] = os.path.join(BASE_DIR, 'documents')  # Chemin absolu pour le dossier 'documents'
+# Configuration de la base de données MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'  # Utilisateur MySQL
+app.config['MYSQL_PASSWORD'] = ''  # Mot de passe MySQL
+app.config['MYSQL_DB'] = 'customers_base'  # Base de données MySQL
+# mysql = MySQL(app)
+
+# Initialisation de l'extension MySQL
+#mysql = MySQL(app)
+
+methods=['POST', 'PUT', 'GET', 'PATCH', 'DELETE']
+
+def get_db():
+    return pymysql.connect(
+        host=app.config['MYSQL_HOST'],
+        user=app.config['MYSQL_USER'],
+        password=app.config['MYSQL_PASSWORD'],
+        db=app.config['MYSQL_DB'],
+        cursorclass=pymysql.cursors.DictCursor
+)
 
 @app.route("/")
-def home():
-    return render_template("home.html")
+# def home():
+#     return render_template("home.html")
 
 @app.route("/documents")
 def documents():
@@ -17,6 +43,54 @@ def settings():
 @app.route("/help")
 def help_page():
     return render_template("help.html")
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+@app.route('/upload', methods=['POST', 'GET'])
+def file_upload():
+    if 'file' not in request.files:
+        return jsonify({'error': 'Aucun fichier détecté dans la requête'}), 400
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return jsonify({'error': 'Aucun fichier sélectionné'}), 400
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
+        file.save(save_path)
+        return jsonify({'message': 'Fichier téléchargé avec succès', 'filename': filename}), 200
+
+    return jsonify({'error': 'Extension de fichier non autorisée'}), 400
+    
+    
+    
+# @app.route('/', methods=['GET'])
+# def list_customers():
+#     per_page = int(request.args.get('per_page', 10))
+#     page = int(request.args.get('page', 1))
+#     offset = (page - 1) * per_page
+    
+#     # Récupération d'une connexion à la base de données && Création d'un curseur pour exécuter des requêtes SQL
+#     cur = get_db().cursor()
+#     cur.execute(f"SELECT * FROM client LIMIT {offset}, {per_page}")
+#     customers = cur.fetchall()
+
+#     # Compter le nombre total de lignes
+#     cur.execute("SELECT COUNT(*) FROM client")
+#     total_customers = 1000 #cur.fetchone()[0]
+    
+#     total_pages = (total_customers // per_page) + (total_customers % per_page > 0)
+    
+#     return render_template('index.html', customers=customers, page=page, per_page=per_page, total_pages=total_pages)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ db/danaya_file.sql
@@ -0,0 +1,157 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:3306
+-- Généré le : lun. 20 jan. 2025 à 16:02
+-- Version du serveur : 8.0.30
+-- Version de PHP : 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `danaya_file`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `files`
+--
+
+CREATE TABLE `files` (
+  `id` int NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `description` text,
+  `file_path` varchar(255) NOT NULL,
+  `uploaded_by` int NOT NULL,
+  `category_id` int NOT NULL,
+  `upload_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `kpi`
+--
+
+CREATE TABLE `kpi` (
+  `id` int NOT NULL,
+  `date` date NOT NULL,
+  `documents_importes` int DEFAULT '0',
+  `reduction_papier` float DEFAULT '0',
+  `volume_numerise` float DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `files`
+--
+ALTER TABLE `files`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `uploaded_by` (`uploaded_by`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- Index pour la table `kpi`
+--
+ALTER TABLE `kpi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `files`
+--
+ALTER TABLE `files`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `kpi`
+--
+ALTER TABLE `kpi`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `files`
+--
+ALTER TABLE `files`
+  ADD CONSTRAINT `files_ibfk_1` FOREIGN KEY (`uploaded_by`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `files_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
